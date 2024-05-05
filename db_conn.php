58c1b8bc22f2aae78cacee84013ccc0c72317a0f
Syed Anas Bukhari <anas14529@gmail.com>
2024-05-05 07:44:03 +0500

    Db

--- db_conn.php
+++ db_conn.php
@@ -2,9 +2,15 @@
 session_start();
 // Database credentials
 $host = "localhost"; // Change this if your database is hosted on a different server
-$username = "root"; // Change this to your MySQL username
-$password = "root"; // Change this to your MySQL password
-$database = "research_work"; // Change this to your MySQL database name
+$username = "u956940883_research_work"; // Change this to your MySQL username
+$password = "hwIdS~1T"; // Change this to your MySQL password
+$database = "u956940883_research_work"; // Change this to your MySQL database name
+
+
+// $host = "localhost"; // Change this if your database is hosted on a different server
+// $username = "root"; // Change this to your MySQL username
+// $password = "root"; // Change this to your MySQL password
+// $database = "research_work"; // Change this to your MySQL database name
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $database);
